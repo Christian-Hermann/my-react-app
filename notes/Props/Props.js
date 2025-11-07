e42bd33791df837b0f4b428f23c4c897c2437124
Your NameChristian <christianhermanndev@gmail.com>
2025-11-07 12:59:17 -0600

    Rendering component's props

--- notes/Props/Props.js
+++ notes/Props/Props.js
@@ -12,3 +12,19 @@ function PropsDisplayer(props) {
 }
 
 export default PropsDisplayer;
+
+// Rendering Component's props
+
+function Product(props) {
+  return (
+    <div>
+      <h1>{props.name}</h1>
+      <h2>{props.price}</h2>
+      <h3>{props.rating}</h3>
+    </div>
+  );
+}
+
+function App() {
+  return <Product name="Apple iWatch" price={500} rating="3.5/5.0" />;
+}
