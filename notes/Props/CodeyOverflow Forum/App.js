2431230a5131f63571726b0789479b9312926ce6
Your NameChristian <christianhermanndev@gmail.com>
2025-11-12 11:59:56 -0600

    Mapping through Card

---
+++ notes/Props/CodeyOverflow Forum/App.js
@@ -0,0 +1,12 @@
+import React from "react";
+import { comments } from "./commentData";
+
+function App() {
+  return (
+    <div>
+      {comments.map((comment) => (
+        <Card commentObject={comment} />
+      ))}
+    </div>
+  );
+}
