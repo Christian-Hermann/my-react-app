23489a59937086494421cbaf526c32b35d3ae855
Your NameChristian <christianhermanndev@gmail.com>
2025-11-12 12:47:38 -0600

    Finished CodeyOverflow Forum

--- notes/Props/CodeyOverflow Forum/App.js
+++ notes/Props/CodeyOverflow Forum/App.js
@@ -1,12 +1,15 @@
 import React from "react";
 import { comments } from "./commentData";
+import Card from "./Card.js";
 
 function App() {
   return (
     <div>
-      {comments.map((comment) => (
-        <Card commentObject={comment} />
+      {comments.map((comment, i) => (
+        <Card key={i} commentObject={comment} />
       ))}
     </div>
   );
 }
+
+export default App;
