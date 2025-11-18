62b7c7c63503293e4fd8307a3cf079f7faa25f3f
Your NameChristian <christianhermanndev@gmail.com>
2025-11-18 13:47:36 -0600

    COntrol when effects are called

--- notes/Hooks/useEffect.js
+++ notes/Hooks/useEffect.js
@@ -18,3 +18,51 @@ export default function Counter() {
     </div>
   );
 }
+
+// Clean Up Effects
+import React, { useState, useEffect } from "react";
+
+export default function Counter() {
+  const [clickCount, setClickCount] = useState(0);
+
+  // your code here
+  const increment = () => setClickCount((prev) => prev + 1);
+
+  useEffect(() => {
+    document.addEventListener("mousedown", increment);
+    return () => {
+      document.removeEventListener('mousedown', increment)
+    }
+  });
+
+  return <h1>Document Clicks: {clickCount}</h1>;
+}
+
+// Control When Effects are Called
+import React, { useState, useEffect } from "react";
+
+export default function Timer() {
+  const [time, setTime] = useState(0);
+  const [name, setName] = useState("");
+
+  useEffect((effect, []) => {
+    const intervalId = setInterval(() => {
+      setTime((prev) => prev + 1);
+    }, 1000);
+
+    return () => {
+      clearInterval(intervalId);
+    };
+  }, []);
+
+const handleChange = ({target}) => {
+  setName(target.value)
+}
+
+  return (
+    <>
+      <h1>Time: {time}</h1>
+      <input value={name} onChange={handleChange} />
+    </>
+  );
+}
