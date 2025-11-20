3bbbab701a391bab88b1d49309f5f385b4c79572
Your NameChristian <christianhermanndev@gmail.com>
2025-11-20 13:33:19 -0600

    Added addThought function and handleTextChange

--- notes/Hooks/Passing Thoughts/AddThoughtForm.js
+++ notes/Hooks/Passing Thoughts/AddThoughtForm.js
@@ -1,36 +1,23 @@
-import React, { useState } from "react";
-import ReactDOM from "react-dom";
-import { AddThoughtForm } from "./AddThoughtForm";
-import { Thought } from "./Thought";
+import { React, useState } from "react";
 import { generateId, getNewExpirationTime } from "./utilities";
 
-export default function App() {
-  const [thoughts, setThoughts] = useState([
-    {
-      id: generateId(),
-      text: "This is a place for your passing thoughts.",
-      expiresAt: getNewExpirationTime(),
-    },
-    {
-      id: generateId(),
-      text: "They'll be removed after 15 seconds.",
-      expiresAt: getNewExpirationTime(),
-    },
-  ]);
+export function AddThoughtForm(props) {
+  const [text, setText] = useState("");
+
+  function handleTextChange(event) {
+    setText(event.target.value);
+  }
 
   return (
-    <div className="App">
-      <header>
-        <h1>Passing Thoughts</h1>
-      </header>
-      <main>
-        <AddThoughtForm />
-        <ul className="thoughts">
-          {thoughts.map((thought) => (
-            <Thought key={thought.id} thought={thought} />
-          ))}
-        </ul>
-      </main>
-    </div>
+    <form className="AddThoughtForm">
+      <input
+        onChange={handleTextChange}
+        value={text}
+        type="text"
+        aria-label="What's on your mind?"
+        placeholder="What's on your mind?"
+      />
+      <input type="submit" value="Add" />
+    </form>
   );
 }
