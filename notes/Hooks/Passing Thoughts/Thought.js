61da7677426a152069cfd95324206854b386ba83
Your NameChristian <christianhermanndev@gmail.com>
2025-11-21 15:25:10 -0600

    Added useEffect to Thought component

--- notes/Hooks/Passing Thoughts/Thought.js
+++ notes/Hooks/Passing Thoughts/Thought.js
@@ -1,7 +1,17 @@
-import React from "react";
+import React, { useEffect } from "react";
 
 export function Thought(props) {
   const { thought, removeThought } = props;
+
+  useEffect(() => {
+    const timeRemaining = thought.expiresAt - Date.now();
+    const timeout = setTimeout(() => {
+      alert("Time has passed!");
+    }, timeRemaining);
+    return () => {
+      clearTimeout(timeout);
+    };
+  }, [thought]);
 
   const handleRemoveClick = () => {
     removeThought(thought.id);
