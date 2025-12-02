055496efe68ca5b94034177bab9d589f449e1367
Your NameChristian <christianhermanndev@gmail.com>
2025-12-02 13:12:14 -0600

    1 - 3 of project

--- notes/React Forms/Saucy Tango Food Order Form/FoodOrderForm.js
+++ notes/React Forms/Saucy Tango Food Order Form/FoodOrderForm.js
@@ -2,8 +2,17 @@ import React, { useState } from "react";
 
 function FoodOrderForm() {
   return (
-    
-  )
+    <form>
+      <label htmlFor="name">Name:</label>
+      <input id="name"></input>
+      <label htmlFor="phone">Phone:</label>
+      <input id="phone"></input>
+      <label htmlFor="address">Address:</label>
+      <input id="address"></input>
+      <label htmlFor="order">Order:</label>
+      <input id="order"></input>
+    </form>
+  );
 }
 
 export default FoodOrderForm;
